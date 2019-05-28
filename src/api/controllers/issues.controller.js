c92b104bdab6c4f5fda8868232a6be60168f9348
Knox316 <fernandocorreia316@gmail.com>
2019-05-28 22:53:38 +0100

    mudado controladores

--- src/api/controllers/issues.controller.js
+++ src/api/controllers/issues.controller.js
@@ -2,22 +2,12 @@ var axios = require("axios");
 const url = "https://redmine-mock-api.herokuapp.com/api/v1/issues?after=2019-04-13T12:00";
 
 
-exports.getAllIssues = async() => {
-    try {
-        const res = await axios.get(url).then(res => console.log(res.data)); 
-    } catch (error) {
-      console.log(error);
-    }
-  }
+exports.getAllIssues = () => {
+  const res = axios.get(url).then(res => console.log(res.data)).catch(err => console.log("Axios err: ", err));
+}
 
-  //corrigir
-  exports.getAllIssuesById = async() => {
-    try {
-        const res = await axios.get(url);
-        const data = res.data;
-        console.log(data.id)  
-  
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
+//corrigir
+exports.getAllIssuesById = () => {
+  const res = axios.get(url).then(res => console.log(res.data.id)).catch(err => console.log("Axios err: ", err));
+}
--- src/api/controllers/users.controller.js
+++ src/api/controllers/users.controller.js
@@ -1,21 +1,12 @@
 var axios = require("axios");
 const url = "https://redmine-mock-api.herokuapp.com/api/v1/users?forceMail=email@address.domain";
 
-exports.getUsers = (async() => {
-    try {
-        const res = await axios.get(url).then(res => console.log(res.data)); 
-    } catch (error) {
-      console.log(error);
-    }
-  })();
+exports.getUsers = () => {
+  const res = axios.get(url).then(res => console.log(res.data)).catch(err => console.log("Axios err: ", err));
 
-  //corrigir
-  exports.getUsersById = async() => {
-    try {
-        const res = await axios.get(url);
-        const data = res.data;
-        console.log(data.id)  
-    } catch (error) {
-      console.log(error);
-    }
-  }
+}
+
+//corrigir
+exports.getUsersById = () => {
+  const res = axios.get(url).then(res => console.log(res.data.id)).catch(err => console.log("Axios err: ", err));
+}
