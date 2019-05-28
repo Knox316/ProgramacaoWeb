d94ca9bc636d75a10bd44ccf51f4f90629f19296
Knox316 <fernandocorreia316@gmail.com>
2019-05-28 17:43:34 +0100

    testing

--- src/api/controllers/users.controller.js
+++ src/api/controllers/users.controller.js
@@ -1,15 +1,13 @@
 var axios = require("axios");
 const url = "https://redmine-mock-api.herokuapp.com/api/v1/users?forceMail=email@address.domain";
 
-exports.getUsers = async() => {
+exports.getUsers = (async() => {
     try {
-        const res = await axios.get(url);
-        const data = res.data;
-        console.log(data)  
+        const res = await axios.get(url).then(res => console.log(res.data)); 
     } catch (error) {
       console.log(error);
     }
-  }
+  })();
 
   //corrigir
   exports.getUsersById = async() => {
