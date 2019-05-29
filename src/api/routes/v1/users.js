3fd7d6613e4e093298c13dff7ea1a2feeb8d79ac
Bruno Rodrigues <a9969@alunos.ipca.pt>
2019-05-29 22:53:55 +0100

    Users

--- src/api/routes/v1/users.js
+++ src/api/routes/v1/users.js
@@ -1,17 +1,12 @@
 var express = require("express");
-var axios = require("axios");
 var router = express.Router();
 const controller = require("../../controllers/users.controller");
 
 
 /* GET ALL USERS*/
-router
-  .route("/")
-  .get(controller.getUsers);
+router.get("/", controller.getUsers);
 
 /* GET USERS BY ID */
-router
-  .get("/:id")
-  .get(controller.getUsersById);
+router.get("/:id", controller.getUsersById);
 
 module.exports = router;
