d0d0498177b7a95c197d78a3888302b8b2fffde0
Knox316 <fernandocorreia316@gmail.com>
2019-05-30 23:21:29 +0100

    added folders

--- src/api/controllers/issues.controller.js
+++ src/api/controllers/issues.controller.js
@@ -18,15 +18,7 @@ exports.getAllIssues = async (req, res) => {
 /** GET ISSUES BY ID */
 exports.getAllIssuesById = async (req, res, next) => {
   try {
-    debugger;
-    const {
-      data
-    } = await axios.get(url)
-    let issues = data.issues;
-    let fid = req.params.id;
-    let id = issues.filter(e => {
-      return e.id == fid
-    })
+
 
     // const issues = issues.filter(e => e.id != id);
     // res.json(issues);
